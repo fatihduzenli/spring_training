332e860d136042ad276cf5403fc1ab12838f82b7
fatihduzenli <fatihdznl1991@gmail.com>
2024-03-23 00:31:29 -0400

    Core-Bean-Scope completed

---
+++ spring-06-Core-BeanScope/src/main/java/com/cydeo/CommentApp.java
@@ -0,0 +1,40 @@
+package com.cydeo;
+
+import com.cydeo.config.CommentConfig;
+import com.cydeo.model.Comment;
+import com.cydeo.service.CommentService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class CommentApp {
+    public static void main(String[] args) {
+
+        Comment comment  = new Comment();
+        comment.setAuthor("Johnson");
+        comment.setText("Spring Framework");
+
+ApplicationContext container = new AnnotationConfigApplicationContext(CommentConfig.class);
+
+CommentService cs1 = container.getBean(CommentService.class);
+CommentService cs2 = container.getBean(CommentService.class);
+
+        System.out.println(cs1);
+        System.out.println(cs2);
+        System.out.println(cs1==cs2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
