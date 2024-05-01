f157a177246cb8d1814749ec99fe87515dd42ee2
fatihduzenli <fatihdznl1991@gmail.com>
2024-05-01 01:27:58 -0400

    Table Creation Completed

---
+++ src/main/java/com/cydeo/entity/Department.java
@@ -0,0 +1,18 @@
+package com.cydeo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Department extends BaseEntity {
+
+    private String department;
+    private String division;
+}
