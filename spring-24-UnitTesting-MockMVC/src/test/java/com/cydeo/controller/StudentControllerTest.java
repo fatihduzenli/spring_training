cb6c636244fc9ec86df57f8d79e10af89442b17c
fatihduzenli <fatihdznl1991@gmail.com>
2024-07-17 23:43:04 -0400

    WebMvcTest created, we tested Student controller

--- spring-24-UnitTesting-MockMVC/src/test/java/com/cydeo/controller/StudentControllerTest.java
+++ spring-24-UnitTesting-MockMVC/src/test/java/com/cydeo/controller/StudentControllerTest.java
@@ -25,15 +25,52 @@ public class StudentControllerTest {
     @MockBean
     private StudentService studentService;
 
-  @Test
+    @Test
     void getStudent_Test() throws Exception {
 
-      mvc.perform(MockMvcRequestBuilders.get("/student")
-              .accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
-              .andExpect(content().json("{\"firstName\": \"Mike\", \"lastName\":  \"Smith\", \"age\": 20}"))
-              .andDo(print()) // this will print it on the console
-              .andReturn();
-  }
+        mvc.perform(MockMvcRequestBuilders.get("/student")
+                        .accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+                .andExpect(content().json("{\"firstName\": \"Mike\", \"lastName\":  \"Smith\", \"age\": 20}"))
+                .andDo(print()) // this will print it on the console
+                .andReturn();
+    }
+
+
+    @Test
+    void jsonAssert_Test() throws Exception {
+        String expected = "{\"firstName\": \"Mike\", \"lastName\":  \"Smith\", \"age\": 20}";
+        String actual = mvc.perform(MockMvcRequestBuilders.get("/student")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andReturn().getResponse().getContentAsString();
+
+
+        // expected-> {"firstName": "Mike", "lastName":  "Smith", "age": 20}
+        // actual -> {"firstName": "Mike", "lastName":  "Smith", "age": 20}
+
+        // when the third parameter false: it passes the test if actual has all the elements that expected contain (do esn't matter if actual has different elements)
+        JSONAssert.assertEquals(expected, actual, true);
+
+    }
+
+    @Test
+    void getStudents_Test() throws Exception {
+        when(studentService.getStudents()).thenReturn(Arrays.asList(
+
+                new StudentDTO("Jhon", "Doe", 20),
+                new StudentDTO("Tom", "Hanks", 50)
+                // Here we are creating stub (behavior for my mock object/StudentService obj)
+        ));
+
+
+        mvc.perform(MockMvcRequestBuilders.get("/students")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(content()
+                        .json("[{\"firstName\": \"Jhon\", \"lastName\":  \"Doe\", \"age\": 20}" +
+                                ", {\"firstName\": \"Tom\", \"lastName\":  \"Hanks\", \"age\": 50}]"))
+                .andReturn();
+
+
+    }
 
 
 }
